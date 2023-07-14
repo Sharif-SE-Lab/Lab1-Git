0e84803df2655a62ded56af4267b60350b0f3366
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2023-07-14 17:27:45 +0430

    feat: init different types of used Queues
    In this simulation, FIFO, Priority, and WRR queue are used.

--- src/queue.py
+++ src/queue.py
@@ -19,5 +19,61 @@ class Queue(ABC):
         pass
 
 
+class FIFOQueue(Queue):
+    def __init__(self, length_limit):
+        self.length_limit = length_limit
+        self.packets: List[Packet] = []
+
+    def push(self, packet: Packet):
+        if self.__get_length() < self.length_limit:
+            self.__push_packet(packet)
+        else:
+            packet.dropped = True
+
+    def pop(self) -> Optional[Packet]:
+        if self.__get_length() > 0:
+            return self.packets.pop(0)
+
+    def __push_packet(self, packet: Packet):
+        self.packets.append(packet)
+
+    def __get_length(self) -> int:
+        return len(self.packets)
 
 
+class PriorityQueue(FIFOQueue):
+    def __push_packet(self, packet: Packet):
+        insert_index = len(self.packets)
+        while (
+            insert_index > 0
+            and self.packets[insert_index - 1].priority < packet.priority
+        ):
+            insert_index -= 1
+        self.packets.insert(insert_index, packet)
+
+
+class WRRQueue(Queue):
+    def __init__(self, length_limit):
+        self.queues = [
+            FIFOQueue(length_limit=length_limit) for _ in range(len(PacketPriority))
+        ]
+        self.priorities = self.get_priorities(PacketPriority)
+        self.turn = 0
+        self.sent_packet = 0
+
+    def get_priorities(self, packet_priority: Type[Enum]) -> List[Enum]:
+        priorities = [priority for priority in packet_priority]
+        priorities.sort(key=lambda e: e.value)
+        return priorities
+
+    def push(self, packet: Packet):
+        self.queues[self.priorities.index(packet.priority)].push(packet)
+
+    def pop(self) -> Optional[Packet]:
+        if self.sent_packet >= self.priorities[self.turn].value:
+            self.turn = (self.turn + 1) % len(self.priorities)
+            self.sent_packet = 0
+        popped_packet = self.queues[self.turn].pop()
+        if popped_packet:
+            self.sent_packet += 1
+        return popped_packet
