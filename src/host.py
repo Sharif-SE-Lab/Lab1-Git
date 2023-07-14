1835c27c4af87aa9f9134c262271e2c4b6a6023a
Ashk Khademian <ashkan.khd.q@gmail.com>
2023-07-14 17:16:00 +0330

    feat: add Host and PacketPriorityProbability host class simulates the host entity.

--- src/host.py
+++ src/host.py
@@ -8,6 +8,12 @@ class PacketPriority(Enum):
     LOW = 5
 
 
+class PacketPriorityProbability(Enum):
+    HIGH = 0.2
+    MEDIUM = 0.3
+    LOW = 0.5
+
+
 class Packet:
 
     def __init__(self, entry_time: float, priority: PacketPriority):
@@ -24,3 +30,17 @@ class Packet:
 
     def get_entry_time(self):
         return self.entry_time
+
+
+class Host:
+    def __init__(self, poisson_parameter):
+        self.poisson_parameter = poisson_parameter
+
+    def generate_packets(self, simulation_time: float) -> List[Packet]:
+        packets = []
+        time = 0
+        while time <= simulation_time:
+            sample_priority = weighted_sample_enum(PacketPriority, PacketPriorityProbability)
+            packets.append(Packet(entry_time=time, priority=sample_priority))
+            time += generate_exponential_variable(1 / self.poisson_parameter, 1)
+        return packets
