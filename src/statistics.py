6b6a982ac7befa07664579a378f1cf9a5c93261e
Ashk Khademian <ashkan.khd.q@gmail.com>
2023-07-14 17:39:25 +0330

    feat: finish all statistical functions

--- src/statistics.py
+++ src/statistics.py
@@ -1,6 +1,9 @@
 from typing import List
 
+import matplotlib.pyplot as plt
+
 from src.host import Packet, PacketPriority
+from src.router import Core
 
 
 def average_queue_length(packets: List[Packet], simulation_time: float) -> float:
@@ -73,3 +76,63 @@ def average_time_in_queue(packets: List[Packet], simulation_time: float, priorit
     return total_time_in_queue / total_packets
 
 
+def average_utilization_of_core(packets: List[Packet], core: Core, simulation_time: float) -> float:
+    time: float = 0
+    for packet in packets:
+        if packet.dropped:
+            continue
+        if packet.executed_by == core:
+            time += packet.service_time
+    return time / simulation_time
+
+
+def count_of_dropped_packets(packets: List[Packet]):
+    count = 0
+    for packet in packets:
+        if packet.dropped:
+            count += 1
+    return count
+
+
+def accumulate_high_priority_packet_diagram(packets: List[Packet], service_policy, simulation_time: float):
+    change_times = []
+    for packet in packets:
+        if packet.dropped or packet.priority.name != 'HIGH':
+            continue
+        elif not packet.service_time:
+            change_times.append(packet.entry_time)
+        else:
+            change_times.append(packet.entry_time)
+            change_times.append(packet.execution_start_time)
+    change_times.append(simulation_time)
+    change_times.sort()
+
+    accumulate_times = []
+    for i in range(0, len(change_times)):
+        time_i = 0
+        for packet in packets:
+            if packet.dropped or packet.priority.name != 'HIGH':
+                continue
+            if packet.service_time:
+                if packet.entry_time <= change_times[i]:
+                    time_i += min(change_times[i], packet.execution_start_time) - packet.entry_time
+            else:
+                if packet.entry_time <= change_times[i]:
+                    time_i += min(change_times[i], simulation_time) - packet.entry_time
+        accumulate_times.append(time_i)
+    # Assuming x and y are your lists
+    x = ['%.2f' % elem for elem in change_times]
+
+    plt.plot(x, accumulate_times)
+    plt.xlabel('time')
+    plt.ylabel('CDF time')
+    plt.title(f'Accumulative time in queue for High priority packets in {service_policy}')
+    plt.show()
+
+
+def count_of_sent_packets(packets: List[Packet]):
+    count = 0
+    for packet in packets:
+        if packet.service_time:
+            count += 1
+    return count
